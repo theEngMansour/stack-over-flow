eda754bfed9f76f85b5c8ad301556df723693851
Mansour Ahmed <mansour.yemen1@gmail.com>
2024-10-13 10:27:55 +0300

    p;

--- app/(root)/export/page.jsx
+++ app/(root)/export/page.jsx
@@ -4,11 +4,12 @@ import React from 'react'
 import { usePDF } from 'react-to-pdf'
 
 export default function Component () {
-  const { toPDF, targetRef } = usePDF({ filename: 'page.pdf' })
+  const { toPDF, targetRef } = usePDF({ filename: 'page.export' })
 
   return (
-    <div className="p-4 w-full h-full bg-accent-blue bg-red-500">
-      <button onClick={() => toPDF()} className="mb-4">Download PDF</button>
+    <div className="p-4 w-full h-full  bg-red-500">
+      <button onClick={() => toPDF()} className="mb-4 bg-red-500">Download PDF
+      </button>
 
       <div ref={targetRef}>
         <h1 className="text-2xl font-bold mb-4">Hello, PDF!</h1>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,51 +1,57 @@
 import "./globals.css";
 import React from "react";
-import { ClerkProvider } from "@clerk/nextjs";
+import {ClerkProvider} from "@clerk/nextjs";
 // eslint-disable-next-line camelcase
-import { Inter, Space_Grotesk } from "next/font/google";
-import { ThemeProvider } from "@/context/ThemeProvider";
-import { Metadata } from "next";
+import {IBM_Plex_Sans_Arabic, Inter, Space_Grotesk} from "next/font/google";
+import {ThemeProvider} from "@/context/ThemeProvider";
+import {Metadata} from "next";
 
 const inter = Inter({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  variable: "--font-inter",
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-inter",
+});
+
+const ibm = IBM_Plex_Sans_Arabic({
+    subsets: ["latin"],
+    weight: ["100", "200", "300"],
+    variable: "--font-ibm",
 });
 
 const spaceGrotesk = Space_Grotesk({
-  subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-spaceGrotesk",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
+    variable: "--font-spaceGrotesk",
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow",
-  description:
-    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
+    title: "DevFlow",
+    description:
+        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    icons: {
+        icon: "/images/site-logo.svg",
+    },
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider
-      appearance={{
-        elements: {
-          formButtonPrimary: "primary-gradient",
-          footerActionLink: "primary-text-gradient hover:text-primary-500",
-        },
-      }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          <ThemeProvider>{children}</ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+    return (
+        <ClerkProvider
+            appearance={{
+                elements: {
+                    formButtonPrimary: "primary-gradient",
+                    footerActionLink: "primary-text-gradient hover:text-primary-500",
+                },
+            }}
+        >
+            <html lang="en">
+            <body className={`${inter.variable} ${ibm.variable} ${spaceGrotesk.variable}`}>
+            <ThemeProvider>{children}</ThemeProvider>
+            </body>
+            </html>
+        </ClerkProvider>
+    );
 }
